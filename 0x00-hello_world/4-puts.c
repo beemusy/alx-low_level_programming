95430ee765aa4fa6b01624aaf72fbd70d1dab7bd
Blessings <beemusy@gmail.com>
2023-02-16 05:50:22 -0800

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ * write a program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ */
+int main(void)
+{
+		puts("\"Programming is like a multilingual puzzle\n");
+		return (0)
+}
