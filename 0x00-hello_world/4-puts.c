85fbaad055948dfaa5aac4d19e47371bccb4795f
Blessings <beemusy@gmail.com>
2023-02-16 10:29:29 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  */
 #include <stdio.h>
 /**
- * main function: write a program that prints exactly with proper grammar,
+ * main:write a program that prints exactly with proper grammar,
  * but the outcome is a piece of art,, followed by a new line.
  *
  * Return:0(Success)
