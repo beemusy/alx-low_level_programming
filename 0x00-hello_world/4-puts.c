3bf47fd79554df34a184af252eb40505a4fc73c3
Blessings <beemusy@gmail.com>
2023-02-16 05:59:46 -0800

    more corrections lol

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * write a program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
  */
 int main(void)
+
 {
-		puts("\"Programming is like a multilingual puzzle\n");
+		puts("\*Programming is like building a multilingual puzzle\n");
 		return (0);
 }
