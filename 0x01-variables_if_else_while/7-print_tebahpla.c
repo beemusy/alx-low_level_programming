859b9a8738bac88d3774bc6bc4ca6be4a701b668
Blessings <beemusy@gmail.com>
2023-02-17 10:49:59 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Blessings Moyo
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *        in reverse, followed by a new line.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	
+	putchar('\n');
+
+
+	return (0);
+}
