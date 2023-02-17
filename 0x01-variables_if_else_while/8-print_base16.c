1c497f09a2c4fb0944903feed0bff9da94fa8bf9
Blessings <beemusy@gmail.com>
2023-02-17 11:53:20 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+/*
+ * File: 8-print_base16.c
+ * Auth: Blessings Moyo
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 16
+ *        in lowercase followed by a new line.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+int num;
+	char letter;
+	
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
