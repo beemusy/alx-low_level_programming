33e01d5e808b10e40b0105b553f913f25ac84454
Blessings <beemusy@gmail.com>
2023-02-20 04:38:01 -0800

    Patience and persistance

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,25 +3,28 @@
  * Auth: Blessings Moyo
  */
 
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single
- *        -digit numbers
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-        int a;
+	int x;
 
-        for (a = 0; a < 10; a++)
-                putchar(((a % 10) + '0' + ',' + ' '));
+	for (x = 0; x <= 9; x++)
+	{
+		putchar((x % 10) + '0');
+		if (x == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-
-        return (0);
+	return (0);
 }
