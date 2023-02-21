9abd6700ad3bcc1350ad11f981b878717bdf6791
Blessings <beemusy@gmail.com>
2023-02-21 02:02:58 -0800

    printing

--- 0x01-variables_if_else_while/0-putchar.c
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -10,7 +10,8 @@
  *
  * Return: Always 0.
 */
-int main(void);
+int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -20,6 +21,7 @@ int main(void);
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
