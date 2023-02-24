b1ff3030729d545ce65ad2ae71d4a2ab3286eee2
Blessings <beemusy@gmail.com>
2023-02-24 12:28:05 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Blessings Moyo
+ */
+
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * @n: input number of times to print '_'
+ * Return: a straight line
+ */
+void print_line(int n)
+{
+	int co;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
