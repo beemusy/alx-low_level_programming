472ab753cdf021876d87c4744989e7d72a0a3ced
Blessings <beemusy@gmail.com>
2023-02-27 11:43:27 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+/*
+ * File: 5-rev_string.c
+ * Auth: Blessings Moyo
+ */
+
+#include "main.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, revs;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		revs = s[i];
+		s[i] = *a;
+		*a = revs;
+		a--;
+	}
+}
