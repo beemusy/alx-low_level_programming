63788cb7b1371bc196de3d20dc76ad796e5c1f74
Blessings <beemusy@gmail.com>
2023-03-11 00:12:21 -0800

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+/*
+ * File: 2-mul.c
+ * Auth: Blessings Moyo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies 2 numbers
+ *
+ * @argc: number of command line arguments
+ * @argv: array containing the program line arguments
+ * Return: 1 when there is an error
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
