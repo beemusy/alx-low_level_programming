95427194f39d95a611d3eafa244c1dd6165b69da
Blessings <beemusy@gmail.com>
2023-03-20 13:22:27 -0700

    task 2 - function to initialise a variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+/*
+ * File: 1-init_dog.c
+ * Auth: Blessings Moyo
+ */
+
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog.
+ * @d: struct to initialize
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
