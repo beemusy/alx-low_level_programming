1e42a0052cf1e17decf894c47fd97bd923a2f74e
Blessings <beemusy@gmail.com>
2023-03-20 07:32:44 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+/*
+ * File: 2-main.c
+ * Auth: Blessings Moyo
+ */
+
+#include <stdio.h>
+/**
+ * main - prints the name of file
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
