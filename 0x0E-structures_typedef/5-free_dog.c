14b35eec53b9553df61270134b688ab65c0894d7
Blessings <beemusy@gmail.com>
2023-03-21 12:39:56 -0700

    task 6 free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+/*
+ * File: 5-free_dog.c
+ * Auth: Blessings Moyo
+ */
+
+#include "dog.h"
+#include <stdlib.h>
+/**
+  * free_dog - frees any memory allocated by dog.
+  * @d: pointer to struct
+  * Return: what does it return?
+  */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
