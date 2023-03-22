a8b60dba5790f6b18ec30f274e8f6c475e3da048
Blessings <beemusy@gmail.com>
2023-03-22 12:35:27 -0700

    first task - print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Blessings Moyo
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to print with
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
