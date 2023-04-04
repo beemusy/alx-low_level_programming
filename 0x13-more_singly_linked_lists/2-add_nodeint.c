c80f629b6d8b0943933db5483f98b2c29c805f64
beemusy <beemusy@gmail.com>
2023-04-04 15:55:21 +0200

    requirements failure correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -21,6 +21,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = *head;
 	*head = malloc(sizeof(listint_t));
 
+
+	if ((*head) == NULL)
+	{
+		return (NULL);
+	}
+
 	(*head)->n = n;
 	(*head)->next = new;
 	return (*head);
