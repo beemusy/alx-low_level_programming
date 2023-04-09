90d31105a7b98fd630f6acda9806315ff8770677
beemusy <beemusy@gmail.com>
2023-04-09 21:14:29 +0200

    bit manipulation task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+/*
+ * File: 0-binary_to_uint.c
+ * Auth: Blessings Moyo
+ */
+
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: points to a string of 0 and 1 characters
+ * Return: the the converted number or 0
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int conv_num;
+	int i;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	conv_num = 0;
+
+	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
+	{
+		conv_num <<= 1;
+		conv_num += b[i] - '0';
+	}
+	return (conv_num);
+}
