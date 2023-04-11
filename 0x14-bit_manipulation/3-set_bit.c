88280cdba9764c7c43d7344179343fa5f4a36c24
beemusy <beemusy@gmail.com>
2023-04-11 16:45:59 +0200

    requirements error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -21,6 +21,11 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	}
 
+	if ((*n << index) == '1')
+	{
+		return (1);
+	}
+
 	if ((*n << index) != '1')
 	{
 		*n = (*n | (1 << index));
