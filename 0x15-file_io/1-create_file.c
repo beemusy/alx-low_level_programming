09d1d1cd97213620fc2cbd9ae39b893ab22a4558
beemusy <beemusy@gmail.com>
2023-04-17 16:34:37 +0200

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int newfile;
 	int len;
-	/*int writefile;*/
+	int writefile;
 
-	newfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	newfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (newfile == -1)
 	{
@@ -32,12 +32,24 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (text_content == NULL)
+	{
+		return (newfile);
+	}
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
 		{
-			write(newfile, text_content, len);
+			writefile = write(newfile, text_content, len);
 		}
+		if (writefile == -1)
+		{
+			close(newfile);
+			return (-1);
+
+		}
+
 	}
 
 	close(newfile);
