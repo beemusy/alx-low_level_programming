6fea698dbead8603ec0c4a27124f87cb8d1ffe66
beemusy <beemusy@gmail.com>
2023-05-18 23:38:00 +0200

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,27 +1,27 @@
 /*
- * File: 0-print_dlistint.c
- * Auth: Blessings Moyo
- */
+* File: 0-print_dlistint.c
+* Auth: Blessings Moyo
+*/
 
 #include <stdlib.h>
 #include "lists.h"
 #include <stdio.h>
 
 /**
- * print_dlistint - prints all the elements of a linked list
- *
- * @dlistint_t: linked list to be printed
- * @h: pointer of header to be printed
- * Return: number of nodes in the linked list
- */
+* print_dlistint - prints all the elements of a linked list
+*
+* @h: linked list to be printed
+*
+* Return: number of nodes in the linked list
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+size_t i;
 
-	for (i = 0; h != NULL; i++)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (i);
+for (i = 0; h != NULL; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
 }
